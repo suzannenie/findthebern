806ba0c6e81eb46cdabd258e8f24a4a2d11c12e0
suzannenie <nie.suzanne01@gmail.com>
2021-01-30 14:46:29 -0500

    deploy

--- levels.py
+++ levels.py
@@ -35,6 +35,3 @@ l21 = Level("find21.jpg", "39,143,69,182", "800px")
 
 Levels = [l1, l2, l3, l4, l5, l6, l7, l8, l9, l10]
 Levels.extend([l11, l12, l13, l14, l15, l16, l17, l18, l19, l20, l21])
-
-
-Levels = [l1, l2, l3, l4, l5, l6, l10]
